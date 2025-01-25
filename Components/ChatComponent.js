6740fa0116af9cf71a30d152ac6981cd2a0356d8
rishita-67 <121110018+Rishita-rm@users.noreply.github.com>
2025-01-25 21:35:45 +0530

    ChatComponent.js

---
+++ Components/ChatComponent.js
@@ -0,0 +1,19 @@
+'use client'
+import Chatbot from "react-chatbot-kit";
+import 'react-chatbot-kit/build/main.css'
+import config from "../config";
+import ActionProvider from "../ActionProvider";
+import MessageParser from "../MessageParser";
+
+function ChatComponent() {
+    return (
+        <div>
+            <Chatbot 
+            config={config}
+            actionProvider={ActionProvider}
+            messageParser={MessageParser}/>
+        </div>
+    );
+}
+
+export default ChatComponent;
